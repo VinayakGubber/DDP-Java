3b55bd3ac9e1d2805c61953d7449df8fafc10ecb
Vinayak Gubber <gubbervinayak@gmail.com>
2025-05-20 23:26:25 +0530

    20/05/25

--- BasicMaths/basicMaths.java
+++ BasicMaths/basicMaths.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 class HelperFunctions {
     public static Scanner sc = new Scanner(System.in);
@@ -76,21 +76,71 @@ class Concepts extends HelperFunctions {
     }
 
     public void divisors() {
+        // In this we have made use of the collection framework in java to store sort
+        // and then print the divisors
+        // The interfaces we use form the java collection framework are present in the
+        // java.util.*; package
 
         System.out.println("Enter the number : ");
         int n = sc.nextInt();
         System.out.println();
-
+        ArrayList<Integer> al = new ArrayList<>();
         System.out.println("The divisors are :-");
         for (int i = 1; i * i <= n; i++) {
             if (n % i == 0) {
-                System.out.println(i + " ");
+                al.add(i);
                 if (n / i != i) {
-                    System.out.println(n / i + " ");
+                    al.add(n / i);
                 }
             }
         }
+        al.sort(null);
+        System.out.println(al);
     }
+
+    public void prime() {
+        // Prime or not OPTIMIZED O(sqrt(n))
+        System.out.println("Enter the number : ");
+        int n = sc.nextInt();
+        System.out.println();
+        int count = 0;
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                count++;
+                if (n / i != i) {
+                    count++;
+                }
+            }
+        }
+        if (count == 2) {
+            System.out.println("Yes a prime number");
+        } else {
+            System.out.println("No not a prime number");
+        }
+    }
+
+    public void gcd() {
+        System.out.println("Enter the number a : ");
+        int a = sc.nextInt();
+        System.out.println();
+        System.out.println("Enter the number b : ");
+        int b = sc.nextInt();
+        System.out.println();
+        while (a > 0 && b > 0) {
+            if (a > b && b != 0) {
+                a = a % b;
+            }
+            if (b > a && a != 0) {
+                b = b % a;
+            }
+        }
+        if (a == 0) {
+            System.out.println("Gcd is : " + b);
+        } else if (b == 0) {
+            System.out.println("Gcd is : " + a);
+        }
+    }
+
 }
 
 public class basicMaths {
@@ -103,7 +153,9 @@ public class basicMaths {
         // operation.count();
         // helper.count(25);
         // helper.power(2, 3);
-        operation.divisors();
+        // operation.divisors();
+        // operation.prime();
+        operation.gcd();
 
     }
 }
